9526543172f191a884802645dcafb2fe615bb727
Katrina Dawap <kattydee3@gmail.com>
2025-07-06 13:30:01 +0100

    hero path update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <section className="hero">
             <div className="container">
-            <img src="/assets/BG.png" alt="Main hero banner" className="hero-image" />
+            <img src="src/assets/BG.png" alt="Main hero banner" className="hero-image" />
                 <div className="hero-text">
                     <p>MINIMAL INTERRIOR DESIGN</p>
                     <h1>We minimize your waste in every step of the process</h1>
